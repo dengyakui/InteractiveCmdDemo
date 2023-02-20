7026fa42f45b1a8e54d35ce1cff093a345b1f01a
dengyakui <dengyakui@outlook.com>
2023-02-20 15:58:58 +0800

    add

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"arg length: {args.Length}");
+for (var i = 0; i < args.Length; i++)
+{
+    Console.WriteLine($"args[{i}] is: {args[i]}");
+}
+Console.WriteLine("input your name:");
+var userName = Console.ReadLine();
+Console.WriteLine($"got user name: {userName}");
+
+Console.WriteLine("input your email:");
+var email = Console.ReadLine();
+Console.WriteLine($"got email: {email}");
+
+Console.WriteLine($"user name: {userName}, email: {email}");
+
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CliWrap;
+using CliWrap.Buffered;
+// 使用以下方式， 每行作为调用命令行的用户输入
+var stdin = """
+			Yoda
+			yoda@gmail.com
+			""";
+var cmd = stdin | Cli.Wrap(@"C:\Users\root\RiderProjects\ConsoleApp1\ConsoleApp1\bin\Debug\net7.0\ConsoleApp1.exe");
+var bufferedCommandResult = await cmd.ExecuteBufferedAsync();
+Console.WriteLine(bufferedCommandResult.StandardOutput);
